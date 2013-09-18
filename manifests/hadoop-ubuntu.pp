b4d47db56648e5b5a0bde4d0c1938fd54dffc419
Tomasz Janeczko <janecztom@gmail.com>
2013-09-18 02:30:44 +0200

    Update hadoop-ubuntu.pp

--- manifests/hadoop-ubuntu.pp
+++ manifests/hadoop-ubuntu.pp
@@ -56,7 +56,7 @@ file { "/home/hduser/.ssh/id_rsa.pub":
 
 ssh_authorized_key { "ssh_key":
   ensure => present,
-  key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC4xZz7A8GKviAr3jnBxPODyOU3czTCV8p55TEKl03bh9af9nkXV57KxeK0eGvz1A6kWENvzQF1X9BojbOvE1kSVF99g4SqP4f4sQY9W+sj8thQxM4p2lmB748XHFnGVq6wC3enGZ8zwTu57CCy8rPJ8joYIUAxBnbriKU9E89JP54fCuptlmSzUFzQ/+vTBtDcj1b8BxKYd2DM3/0CuHY3Lejk6FBlyaf0e14+ENQjgsNBk3d3SyfINkXNQjA/zax6Qqq8j9TpC3Aa1rEKDUPayw5CrEQV5NsSclyfDDInUNsdEbFgWC2+HMEQpOSo7K7anzrfmA7bpto2AXF5zu4z",
+  key => "CHANGE_THIS_KEY_ACCORDINGLY!",
   type => "ssh-rsa",
   user => hduser,
   require => File['/home/hduser/.ssh/id_rsa.pub']
